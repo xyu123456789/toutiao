f479c80c3bf3d3458844477d90dfa9758db72741
chenxianduan <1140387046@qq.com>
2022-08-27 16:06:20 +0800

    123

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 自动的打开浏览器，指定端口和服务器port,host
+  // 关闭eslint检查
+  lintOnSave: false,
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    }
+  }
 })
